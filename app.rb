4d8849748b127ac3e7a725081dabd4b9a23c3530
tryangul <ryan.broughan@gmail.com>
2015-08-14 11:46:09 -0700

    add /add_word view and post route

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ require './lib/word'
 require './lib/definition'
 
 get('/') do
-@words = Word.all
-erb :index
+  @words = Word.all
+  erb :index
+end
+
+get('/add_word') do
+  @words = Word.all
+  erb :add_word
+end
+
+post('/') do
+  @words = Word.all
+
+  word_name = params.fetch('word_name')
+  definition1 = params.fetch('definition1')
+  word = Word.new({name: word_name, definitions: [definition1]})
+  word.save
+  erb :index
 end
--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -1,11 +1,20 @@
 require 'capybara/rspec'
 require './app'
-require 'sinatra'
 Capybara.app = Sinatra::Application
 
 describe('when using the dictionary site', { :type => :feature }) do
   it('shows the index page with a list of words') do
     visit '/'
     expect(page).to have_content('My Dictionary')
+  end
+end
+
+describe('when adding a word', { :type => :feature }) do
+  it('shows the index page with a list of words') do
+    visit '/add_word'
+    fill_in 'word_name', with: 'Taco'
+    fill_in 'definition1', with: 'my lunch'
+    click_button 'Add'
+    expect(page).to have_content('Taco')
   end
 end
