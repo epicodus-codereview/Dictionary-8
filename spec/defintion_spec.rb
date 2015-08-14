a30ef8c51579ae92a0500d9a54f737462aa4f308
tryangul <ryan.broughan@gmail.com>
2015-08-14 10:23:58 -0700

    add first spec

---
+++ spec/defintion_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require 'defintion'
+
+describe Definition do 
+  
+  before do
+  	@test_definition = Definition.new('the meaning of a word')
+  end
+
+  describe '#statement'
+	it 'returns the formal statement of the Definition' do
+  	  expect(@test_definition.statement).to eq 'the meaning of a word'
+    end
+  end
+
+  
+end
